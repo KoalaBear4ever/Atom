31c0a4eed12c2f0d83f55f0135d6cc49ebf61ff4
KoalaBear4ever <mrx.mistik@mail.ru>
2022-08-09 17:00:38 +0600

    Start

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import time
+import datetime
+import click
+
+
+# Указываем папку в которой производить поиск
+path='D:\Тест 1'
+
+for rootdir, dirs, files in os.walk(path):
+    for file in files:
+        if((file.split('.')[-1])=='txt'):
+            pathfile = os.path.join(rootdir, file)
+            TimePath = time.strftime("%d.%m.%y", time.gmtime(os.path.getmtime(pathfile)))
+            today = datetime.datetime.today().strftime("%d.%m.%y")
+            if TimePath == today:
+                filename = file
+                TimeArch = time.strftime("%Y%m%d", time.gmtime(os.path.getmtime(pathfile)))
+                mainDir = rootdir
+                break
+                
+
+rootDirArch = mainDir + "\Архив"
+# Папки из которой и в которую будет происходить перемещение
+moveFrom = f"{mainDir}\{filename}"
+moveTo = f"{rootDirArch}\{filename}"
+moveTo = '.'.join(moveTo.split('.')[:-1])
+moveTo = f"{moveTo}({TimeArch}).txt"
+# Копируем файл из папки в папку
+#shutil.copyfile(moveFrom, moveTo, follow_symlinks=True)
+
+#SearchTest = "C:\Program Files\1cv8\8.3.15.1869\bin\chdbfl.exe"
+click.launch("D:\Тест 1\Архив\TEST(20220809).txt")
