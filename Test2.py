86f097481a851745c01177db06bde55f21012083
KoalaBear4ever <mrx.mistik@mail.ru>
2022-08-10 17:30:21 +0600

    upd

--- Test2.py
+++ Test2.py
@@ -6,4 +6,4 @@ app = Application(backend="uia").connect(path=r"C:\Program Files (x86)\1cv8\8.3.
 dlg = app['Проверка физической целостности файла БД']
 dlg.print_control_identifiers()
 app.dlg.control
-print(dlg.pane3.Static2.wrapper_object().minimize())
+print(dlg.pane3.Static2.wrapper_object())
